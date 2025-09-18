8b906984d58b4b60f00abb594381b4e8891ce9cb
Naman Pal <namanpal0204@gmail.com>
2025-09-18 15:12:55 +0530

    First commits ---

---
+++ 1.js
@@ -0,0 +1,46 @@
+// function f1(use=guest){
+//     console.log('hello ${user}');
+// }
+// f1();
+// f1(Ashish);
+// consr add= (a,b)
+// function sum(a,b)
+// {
+//     console.log("before return");
+//         return (a+b);
+//             console.log("after return");
+// }
+// console.log(sum(10,20));
+// function(){
+
+// }
+// // callback function
+// function f1(user, login)
+// {
+//     console.log('hello $ {user}');
+// login ();
+// }
+// function login (){
+
+// }
+
+// Asynchronous programming
+waitfortwoseconds(){
+    const ms = new Date().getline()+2000;
+    while (newDate().getline()< ms)
+}
+function register(){
+    waitfortwoseconds();
+    console.log('register end');
+}
+// waitfortwoseconds(){
+//     court ms = new Date().getMilliseconds()+2000;
+//     while(new Date().getMilliseconds()<ms)
+// }
+register();
+sendEmail();
+login();
+Getdata();
+displaydata();
+console.log('other application');
+
